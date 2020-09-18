d917e5167962cee40423bc909ffda9b63ebe04f8
Cmlynars <chris@mojay.co>
2020-09-18 15:17:55 +0800

    new word, styling, and readme update

--- src/words.js
+++ src/words.js
@@ -1,32 +1,110 @@
 const words = [{
     "chinese": "你好",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "pinyin": "nǐ hǎo",
+    "definition": "hello"
 }, {
-    "chinese": "屁股",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "chinese": "和谐",
+    "pinyin": "hé xié",
+    "definition": "harmony"
 }, {
-    "chinese": "牛奶",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "chinese": "石头",
+    "pinyin": "jié shí",
+    "definition": "stone, rock"
 }, {
-    "chinese": "发脾",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "chinese": "生活",
+    "pinyin": "shēng huó",
+    "definition": "live"
 }, {
-    "chinese": "摩羯",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "chinese": "尊严",
+    "pinyin": "zūn yán",
+    "definition": "dignity, honor"
 }, {
     "chinese": "振聋发聩",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "pinyin": "zhèn lóng fā kuì",
+    "definition": "shocked, \"so loud even the deaf can hear\"" 
 }, {
-    "chinese": "抹黑",
-    "pinyin": "nihao",
-    "definition": "Hello"
+    "chinese": "拍马屁",
+    "pinyin": "pāi mǎ pì",
+    "definition": "suck-up, brown noser"
 },
+{
+    "chinese": "傻蛋",
+    "pinyin": "shǎ dàn",
+    "definition": "stupid egg (insult)"
+},
+{
+    "chinese": "牛逼",
+    "pinyin": "niú bī",
+    "definition": "awesome, \"cow's vagina\""
+},
+{
+    "chinese": "凹凸",
+    "pinyin": "āo tū",
+    "definition": "uneven, bumpy"
+},
+{
+    "chinese": "新加坡",
+    "pinyin": "xīn jiā pō",
+    "definition": "Singapore"
+},
+{
+    "chinese": "芝加哥",
+    "pinyin": "zhī jiā gē",
+    "definition": "Chicago"
+},
+{
+    "chinese": "吉姆",
+    "pinyin": "jí mǔ",
+    "definition": "Jim"
+},
+{
+    "chinese": "史考特",
+    "pinyin": "shǐ kǎo tè",
+    "definition": "Scott"
+},
+{
+    "chinese": "乒乓",
+    "pinyin": "pīng pāng",
+    "definition": "Ping pong"
+},
+{
+    "chinese": "囧",
+    "pinyin": "jiǒng",
+    "definition": "(used as suprise emoji)"
+},
+{
+    "chinese": "龘",
+    "pinyin": "tà",
+    "definition": "sound of a dragon walking"
+},
+{
+    "chinese": "鬻",
+    "pinyin": "yù",
+    "definition": "to sell one's children to buy gruel"
+},
+{
+    "chinese": "鬱",
+    "pinyin": "yù",
+    "definition": "gloomy, depressed"
+},
+{
+    "chinese": "尷尬",
+    "pinyin": "gān gà",
+    "definition": "awkward"
+},
+{
+    "chinese": "克里斯",
+    "pinyin": "kè lǐ sī",
+    "definition": "Chris"
+}, {
+    "chinese": "成功",
+    "pinyin": "chéng gōng",
+    "definition": "success"
+}, {
+    "chinese": "码农",
+    "pinyin": "mǎ nóng",
+    "definition": "programmer, \"code farmer\""
+}
 ]
 
 export default words
