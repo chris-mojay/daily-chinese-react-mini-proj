3c21384158b7f7ccf8a46af1bc2cc613d4524f37
Cmlynars <chris@mojay.co>
2020-09-18 14:24:16 +0800

    Updates

---
+++ precache-manifest.6de65f80a0cc90d11a3dde97e2bf3666.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d9a6d4827f376bd751801b65be1e0c",
+    "url": "/daily-chinese-react-mini-proj/index.html"
+  },
+  {
+    "revision": "9b890c8a6e639cbb946e",
+    "url": "/daily-chinese-react-mini-proj/static/css/main.2161fd41.chunk.css"
+  },
+  {
+    "revision": "ffa39c304b35a08911fd",
+    "url": "/daily-chinese-react-mini-proj/static/js/2.40f440ed.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/daily-chinese-react-mini-proj/static/js/2.40f440ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b890c8a6e639cbb946e",
+    "url": "/daily-chinese-react-mini-proj/static/js/main.ec6d43ae.chunk.js"
+  },
+  {
+    "revision": "3925f152ee73fc151b39",
+    "url": "/daily-chinese-react-mini-proj/static/js/runtime-main.3b9ee758.js"
+  },
+  {
+    "revision": "90431bdb93381fa92d4d9f4a76fe780e",
+    "url": "/daily-chinese-react-mini-proj/static/media/char_bg.90431bdb.png"
+  },
+  {
+    "revision": "e22b7827aea1298f51bfaa9574ce9a96",
+    "url": "/daily-chinese-react-mini-proj/static/media/daily_chinese_horizontal.e22b7827.svg"
+  }
+]);
